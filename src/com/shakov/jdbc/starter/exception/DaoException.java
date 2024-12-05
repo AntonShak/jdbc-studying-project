c86ead68877ffad52db97db77be2cc2d412e2e85
Anton Shakov <annawinnern1@gmail.com>
2024-12-06 01:02:03 +0300

    my jdbc studying

---
+++ src/com/shakov/jdbc/starter/exception/DaoException.java
@@ -0,0 +1,8 @@
+package com.shakov.jdbc.starter.exception;
+
+public class DaoException extends RuntimeException {
+
+    public DaoException(Throwable throwable) {
+        super(throwable);
+    }
+}
